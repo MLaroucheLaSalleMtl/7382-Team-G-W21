c682d45bad9d616c124f73357295793e7f6daa11
Jin Chen <ac.chencho@gmail.com>
2021-04-11 21:46:20 -0400

    + Adding spawner scripts for instantiating items. + Adding prefabs with items. * Commenting wrong code in PlayerGUI.

--- Assets/Scripts/PlayerGUI.cs
+++ Assets/Scripts/PlayerGUI.cs
@@ -25,23 +25,24 @@ public class PlayerGUI : MonoBehaviour
     float curStamina = 100;
     private void Update()
     {
-        tempHunger -= Time.deltaTime;
-        if (tempHunger<=0)
-        {
-            tempHunger = 2;
-            curHunger -= 5;
-            if (curHunger <= 0)
-            {
-                curHunger = 0;
-                tips.SetActive(true);
-                tips.GetComponentInChildren<Text>().text = "GameOver!!";
-            }
-            //maxHunger = maxHunger <= 0 ? 0 : maxHunger;
-        }
-        UpdateHunger();
-        //刷新health 和stamina值
-        UpdateHealth(curHunger);
-        UpdateStamina(curStamina);
+        // TODO::Fix this. This is not well done
+        //tempHunger -= Time.deltaTime;
+        //if (tempHunger<=0)
+        //{
+        //    tempHunger = 2;
+        //    curHunger -= 5;
+        //    if (curHunger <= 0)
+        //    {
+        //        curHunger = 0;
+        //        tips.SetActive(true);
+        //        tips.GetComponentInChildren<Text>().text = "GameOver!!";
+        //    }
+        //    //maxHunger = maxHunger <= 0 ? 0 : maxHunger;
+        //}
+        //UpdateHunger();
+        ////刷新health 和stamina值
+        //UpdateHealth(curHunger);
+        //UpdateStamina(curStamina);
     }
     public void UpdateHealth(float HP )
     {
@@ -65,16 +66,31 @@ public class PlayerGUI : MonoBehaviour
         curHunger = curHunger >= 100 ? 100 : curHunger;// to limit the maximum value to 100
     }
 
+    /// <summary>
+    /// Updating HP
+    /// </summary>
+    /// <param name="HP"></param>
+    /// <param name="maxHP"></param>
     public void UpdateHealth(float HP, float maxHP)
     {
         this.health.fillAmount = HP / maxHP;
     }
 
+    /// <summary>
+    /// Updating Stamina
+    /// </summary>
+    /// <param name="stamina"></param>
+    /// <param name="maxStamina"></param>
     public void UpdateStamina(float stamina, float maxStamina)
     {
         this.stamina.fillAmount = stamina / maxStamina;
     }
 
+    /// <summary>
+    /// Updating Hunger
+    /// </summary>
+    /// <param name="hunger"></param>
+    /// <param name="maxHunger"></param>
     public void UpdateHunger(float hunger, float maxHunger)
     {
         this.hunger.fillAmount = hunger / maxHunger;
